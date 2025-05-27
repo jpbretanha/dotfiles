34c5c10e8ab4f8bf66e8c2244abef9ed46fe3366
João Pedro Bretanha <jpbretanha@gmail.com>
2025-05-27 14:23:13 -0300

    add .zshrc.local to synlinked files

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.zshrc.local ~/.zshrc.local
 ln -sf ~/dotfiles/karabiner/karabiner.json ~/.config/karabiner/karabiner.json
 ln -sf ~/dotfiles/nvim/better-vim.lua ~/.config/better-vim/better-vim.lua
